964078697e608cdd62d1e2586f8441f1e645b31c
Uday-lal <udaylal014@gmail.com>
2021-10-24 00:12:45 +0530

    Add .gitignore and setup python backend

---
+++ server/model.py
@@ -0,0 +1,47 @@
+import mnist  # pip install mnist
+from sklearn.svm import LinearSVC
+import pickle
+
+# MNIST module provide mnist dataset.
+# But the problem is it has three dimensional array so you need to make it into correct shape before start using it.
+
+
+X_train = mnist.train_images()
+y_train = mnist.train_labels()
+X_test = mnist.test_images()
+y_test = mnist.test_labels()
+
+# Shaping the datasets
+n_sample, nx, ny = X_train.shape
+n_sample_test, nx_test, ny_test = X_test.shape
+clean_datasets = X_train.reshape((n_sample, nx * ny))
+clean_x_test = X_test.reshape((n_sample_test, nx_test * ny_test))
+
+# Start training
+svm = LinearSVC()
+svm.fit(clean_datasets, y_train)
+print(svm.score(clean_datasets, y_train))  # Printing the accuracy
+
+
+def save_model(model_obj):
+    """
+    For saving the model
+    :param model_obj: AI algorithm.
+    :return: None
+    """
+    file = open("mnist.pickle", "wb")
+    pickle.dump(model_obj, file)
+    return None
+
+
+def prediction(data):
+    """
+    Making prediction.
+    :returns: list of predicted number
+    :param data: Input data or testing data
+    :return: list
+    """
+    file = open("mnist.pickle", "rb")
+    model = pickle.load(file)
+    predict = model.predict(data)
+    return predict
